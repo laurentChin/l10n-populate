cbda8ab104477dd98deb900fee61a5b839c1e64f
Laurent Janet <laurent.janet@gmail.com>
2018-02-26 21:23:26 +0100

    adds directory content validation

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const program = require('commander');
 const colors = require('colors/safe');
 
 const path = require('path');
+const fs = require('fs');
 const inputValidator = require('./src/validators/input');
 
 program
@@ -14,7 +15,22 @@ program
   .option('-o, --output [directory]', 'The name of the directory where you want to save the generated file(s)', 'dist')
   .parse(process.argv);
 
-if (!inputValidator.isValid(path.join(__dirname, program.input))) {
-  console.log(colors.red(`The program will now exit because '${program.input}' doesn't seems to be a valid file or directory`));
+const absolutePath = path.join(__dirname, program.input);
+const invalidInputErrorMessage = colors.red(`The program will now exit because '${program.input}' doesn't seems to be or contains valid file(s)`);
+
+// Check that the input is a valid file (name and extension) or a directory
+if (!inputValidator.isValid(absolutePath)) {
+  console.log(invalidInputErrorMessage);
   process.exit(1);
+}
+
+// If the input is a directory check that it contains valid files
+if (fs.statSync(absolutePath).isDirectory()) {
+  fs.readdirSync(absolutePath)
+    .forEach((file) => {
+      if(inputValidator.isValid(path.join(absolutePath, file))) {
+        console.log(invalidInputErrorMessage);
+        process.exit(1);
+      }
+    });
 }
