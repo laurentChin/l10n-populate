efbfe9867cf3fc0bd48b56d8dbea351f5611b989
Laurent Janet <laurent.janet@gmail.com>
2018-04-02 15:23:15 +0200

    implements --template option validation

--- index.js
+++ index.js
@@ -9,28 +9,34 @@ const fs = require('fs');
 
 const inputValidator = require('./src/validators/input');
 const contentValidator = require('./src/validators/content');
+const templateValidator = require('./src/validators/template');
+
+const invalidPathErrorMessage = require('./src/errors/invalidPathErrorMessage');
 
 program
   .version('1.0.0', '-v, --version')
   .description('A command line tool to inject translations into static files from json')
   .option('-i, --input [file|directory]', 'The file or directory where the l10n content is stored', 'l10n')
+  .option('-t, --template [file|directory]', 'The file or directory where the l10n content will be injected', '.')
   .option('-o, --output [directory]', 'The name of the directory where you want to save the generated file(s)', 'dist')
   .parse(process.argv);
 
-const absolutePath = path.join(process.env.PWD, program.input);
-const invalidInputErrorMessage = colors.red(`The program will now exit because '${program.input}' doesn't seems to be or contains valid file(s)`);
+const inputAbsolutePath = path.join(process.env.PWD, program.input);
+const templateAbsolutePath = path.join(process.env.PWD, program.template);
+const invalidInputErrorMessage = colors.red(invalidPathErrorMessage.generate(program.input));
+const invalidTemplateErrorMessage = colors.red(invalidPathErrorMessage.generate(program.template));
 
 // Check that the input is a valid file (name and extension) or a directory
-if (!inputValidator.isValid(absolutePath)) {
+if (!inputValidator.isValid(inputAbsolutePath)) {
   console.log(invalidInputErrorMessage);
   process.exit(1);
 }
 
 // If the input is a directory check that it contains valid files
-if (fs.statSync(absolutePath).isDirectory()) {
-  fs.readdirSync(absolutePath)
+if (fs.statSync(inputAbsolutePath).isDirectory()) {
+  fs.readdirSync(inputAbsolutePath)
     .forEach((file) => {
-      const filePath = path.join(absolutePath, file);
+      const filePath = path.join(inputAbsolutePath, file);
       let isValid = inputValidator.isValid(filePath); // validate the filename and extension
 
       if (isValid && !contentValidator.isValid(fs.readFileSync(filePath))) { // validate the file content
@@ -41,7 +47,13 @@ if (fs.statSync(absolutePath).isDirectory()) {
 }
 
 // If the input is a file check that it contains a JSON content
-if (!fs.statSync(absolutePath).isDirectory() && !contentValidator.isValid(fs.readFileSync(absolutePath))) {
+if (!fs.statSync(inputAbsolutePath).isDirectory() && !contentValidator.isValid(fs.readFileSync(inputAbsolutePath))) {
   console.log(invalidInputErrorMessage);
+  process.exit(1);
+}
+
+// Check that the template option is an .html file or a directory containing html file(s)
+if (!templateValidator.isValid(templateAbsolutePath)) {
+  console.log(invalidTemplateErrorMessage);
   process.exit(1);
 }
