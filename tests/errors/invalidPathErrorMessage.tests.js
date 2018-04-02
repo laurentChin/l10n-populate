b1f76c94509c83b885390d6f98ca5bd4b2605216
Laurent Janet <laurent.janet@gmail.com>
2018-04-02 15:36:03 +0200

    cs

--- tests/errors/invalidPathErrorMessage.tests.js
+++ tests/errors/invalidPathErrorMessage.tests.js
@@ -5,6 +5,12 @@ const invalidPathErrorMessage = require('../../src/errors/invalidPathErrorMessag
 test('the message must depend on the path given', t => {
   t.plan(2);
 
-  t.equal(invalidPathErrorMessage.generate('input'), 'The program will now exit because \'input\' doesn\'t seems to be or contains valid file(s)');
-  t.equal(invalidPathErrorMessage.generate('another_input'), 'The program will now exit because \'another_input\' doesn\'t seems to be or contains valid file(s)');
+  t.equal(
+    invalidPathErrorMessage.generate('input'),
+    'The program will now exit because \'input\' doesn\'t seems to be or contains valid file(s)'
+  );
+  t.equal(
+    invalidPathErrorMessage.generate('another_input'),
+    'The program will now exit because \'another_input\' doesn\'t seems to be or contains valid file(s)'
+  );
 });
